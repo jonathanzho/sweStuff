7cc34a25eb79734cd81a489caa2e98ce987a714c
Jonathan Zhong <33461789+jonathanzho@users.noreply.github.com>
2019-02-01 23:29:22 -0800

    java: Added TestReverseString.java

--- java/algoDS/reverseString/TestReverseString.java
+++ java/algoDS/reverseString/TestReverseString.java
@@ -6,25 +6,32 @@ public class TestReverseString {
 
     public static String reverseString(String inWord) {
         String outWord = null;
+
         if (inWord != null) {
             System.out.println("[D] reverseString: inword=[" + inWord + "]");
         } else {
             System.out.println("[E] reverseString: inWord == null !!!");
             return outWord;
         }
+
         int len = inWord.length(); 
         if (len <= 1) {
             System.out.println("[I] reverseString: len <= 1. No need to reverse.");
             outWord = inWord;
             return outWord;
         }
+
+	char[] inWordArray = inWord.toCharArray();
         for (int i = 0; i < len/2; i++) {
             char temp = inWord.charAt(i);
-	    inWord[i] = inWord[len-1-i];
-	    inWord[len-1-i] = temp;
+	    inWordArray[i] = inWordArray[len-1-i];
+	    inWordArray[len-1-i] = temp;
 	    i++;
         }
+
+	outWord = new String(inWordArray);
         System.out.println("[V] reverseString: outWord=[" + outWord + "]");
+
         return outWord;
     }
 }
