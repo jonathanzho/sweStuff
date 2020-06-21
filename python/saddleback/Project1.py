f069631d7b9821b6b9339fb3d4ad63b29d9f0b50
Jonathan Zhong <jonathanzhong@macbookpro2017.local>
2020-06-21 16:17:39 -0700

    python/saddleback/Project1.py

---
+++ python/saddleback/Project1.py
@@ -0,0 +1,40 @@
+# =========
+# Project 1
+# =========
+
+bestInvestorName = ''
+bestInvestorGain = 0.0
+
+for i in range(5):
+    investorName = input('Enter inverstor name: ')
+    stockPurchasePrice = float(input('Enter stock purchase price: '))
+    numStocksPurchased = int(input('Enter number of stocks purchased: '))
+
+    stockSalePrice = float(input('Enter stock sale price: '))
+    numStocksSold = int(input('Enter number of stocks sold: '))
+
+    purchaseTotalNoCommission = numStocksPurchased * stockPurchasePrice
+    purchaseCommission =  purchaseTotalNoCommission * 0.03
+    purchaseTotal = purchaseTotalNoCommission + purchaseCommission
+    saleTotalNoCommission = numStocksSold * stockSalePrice
+    saleCommission = saleTotalNoCommission * 0.03
+    saleTotal = saleTotalNoCommission - saleCommission
+    investorGain = saleTotal - purchaseTotal
+    print(investorName, purchaseTotalNoCommission, saleTotalNoCommission, investorGain)
+
+    print(purchaseCommission, saleCommission)
+
+    if (investorGain > bestInvestorGain):
+        bestInvestorGain = investorGain
+        bestInvestorName = investorName
+
+print(bestInvestorName, bestInvestorGain)
+
+
+
+
+    
+    
+                                     
+                                
+                         
