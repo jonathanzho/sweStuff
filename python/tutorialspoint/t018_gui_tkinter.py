9b5367e65d1c6153cd9badd2e7e83e80efcb4788
Jonathan Zhong <jonathanzhong@macbookpro2017.local>
2020-07-31 16:55:12 -0700

    python/tutorialspoint/t018_gui_tkinter.py: added class MainApplication for convention.

--- python/tutorialspoint/t018_gui_tkinter.py
+++ python/tutorialspoint/t018_gui_tkinter.py
@@ -3,20 +3,28 @@ import tkinter as tk
 def handle_button1_click(event):
     print('button1 was clicked!')
 
+class MainApplication(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+    
+        frame = tk.Frame(parent)
+        frame.pack()
+
+        button1 = tk.Button(frame,
+            text='Click me!',
+            width=20,
+            height=10,
+            bg='white',
+            fg='red')
+        button1.pack()
+        button1.bind('<Button-1>', handle_button1_click)
+
 def main():
     root = tk.Tk()
 
-    frame = tk.Frame(root)
-    frame.pack()
-
-    button1 = tk.Button(frame,
-        text='Click me!',
-        width=20,
-        height=10,
-        bg='white',
-        fg='red')
-    button1.pack()
-    button1.bind('<Button-1>', handle_button1_click)
+    app = MainApplication(root)
+    app.pack(side='top', fill='both', expand=True)
 
     root.mainloop()
 
