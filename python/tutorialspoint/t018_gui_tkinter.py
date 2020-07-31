d956fcda94f1ad00c30f3687602ecb65b8fc9a38
Jonathan Zhong <jonathanzhong@macbookpro2017.local>
2020-07-31 16:37:05 -0700

    python/tutorialspoint/t018_gui_tkinter.py: implement main() function as a convention.

--- python/tutorialspoint/t018_gui_tkinter.py
+++ python/tutorialspoint/t018_gui_tkinter.py
@@ -3,19 +3,22 @@ import tkinter as tk
 def handle_button1_click(event):
     print('button1 was clicked!')
 
-window = tk.Tk()
+def main():
+    root = tk.Tk()
 
-frame = tk.Frame(window)
-frame.pack()
+    frame = tk.Frame(root)
+    frame.pack()
 
-button1 = tk.Button(frame,
-    text='Click me!',
-    width=20,
-    height=10,
-    bg='white',
-    fg='red'
-    )
-button1.pack()
-button1.bind('<Button-1>', handle_button1_click)
+    button1 = tk.Button(frame,
+        text='Click me!',
+        width=20,
+        height=10,
+        bg='white',
+        fg='red')
+    button1.pack()
+    button1.bind('<Button-1>', handle_button1_click)
 
-window.mainloop()
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
