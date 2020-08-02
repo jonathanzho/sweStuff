5fef08e7d1b7f9f51d17b9e8baa16f46995671d5
Jonathan Zhong <jonathanzhong@macbookpro2017.local>
2020-08-02 01:39:16 -0700

    python/saddleback/Project3.py: button click callback: insert line to file.

--- python/saddleback/Project3.py
+++ python/saddleback/Project3.py
@@ -2,6 +2,7 @@
 # Project 3
 # =========
 
+import os
 import tkinter as tk
 
 class Employee:
@@ -9,7 +10,19 @@ class Employee:
         self.name = name
         self.idNumber = idNumber
         self.department = department
-        self.jobnTitle = jobTitle
+        self.jobTitle = jobTitle
+
+    def insert_file(self):
+        if not self.name:
+            # Exit a GUI program:
+            os._exit(0)
+        file = open('Project3_Employees.txt', 'a')
+        file.write('{},{},{},{}\n'.format(
+            self.name,
+            self.idNumber,
+            self.department,
+            self.jobTitle))
+        file.close()
 
 class MainApplication(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
@@ -23,44 +36,60 @@ class MainApplication(tk.Frame):
                              justify='center')
         labelName.grid(row=0, column=0)
 
-        entryName = tk.Entry(gridFrame,
+        self.entryName = tk.Entry(gridFrame,
                              bd=5)
-        entryName.grid(row=0, column=1)
+        self.entryName.grid(row=0, column=1)
 
         labelIdNumber = tk.Label(gridFrame,
                              text='ID Number',
                              justify='center')
         labelIdNumber.grid(row=1, column=0)
 
-        entryIdNumber = tk.Entry(gridFrame,
+        self.entryIdNumber = tk.Entry(gridFrame,
                              bd=5)
-        entryIdNumber.grid(row=1, column=1)
+        self.entryIdNumber.grid(row=1, column=1)
 
         labelDepartment = tk.Label(gridFrame,
                              text='Department',
                              justify='center')
         labelDepartment.grid(row=2, column=0)
 
-        entryDepartment = tk.Entry(gridFrame,
+        self.entryDepartment = tk.Entry(gridFrame,
                              bd=5)
-        entryDepartment.grid(row=2, column=1)
+        self.entryDepartment.grid(row=2, column=1)
 
         labelJobTitle = tk.Label(gridFrame,
                              text='Job Title',
                              justify='center')
         labelJobTitle.grid(row=3, column=0)
 
-        entryJobTitle = tk.Entry(gridFrame,
+        self.entryJobTitle = tk.Entry(gridFrame,
                              bd=5)
-        entryJobTitle.grid(row=3, column=1)
+        self.entryJobTitle.grid(row=3, column=1)
 
         buttonAddToFile = tk.Button(gridFrame,
                                     borderwidth=5,
                                     fg='red',
                                     text='Add to File')
         buttonAddToFile.grid(row=4, column=1)
+        buttonAddToFile.bind('<Button-1>', self.buttonAddToFile_cb)
 
         gridFrame.pack(side='top')
+
+    def buttonAddToFile_cb(self, event):
+        name = self.entryName.get()
+        idNumber = self.entryIdNumber.get()
+        department = self.entryDepartment.get()
+        jobTitle = self.entryJobTitle.get()
+
+        emp = Employee(name, idNumber, department, jobTitle)
+        emp.insert_file()
+
+        # Clear entry values:
+        self.entryName.delete(0, 'end')
+        self.entryIdNumber.delete(0, 'end')
+        self.entryDepartment.delete(0, 'end')
+        self.entryJobTitle.delete(0, 'end')
 
 def main():
     root = tk.Tk()
@@ -69,6 +98,12 @@ def main():
     
     app = MainApplication(root)
     app.pack(side="top", fill="both", expand=True)
+
+    # Remove old file if it exists:
+    try:
+        os.remove('Project3_Employees.txt')
+    except OSError:
+        pass
     
     root.mainloop()
 
